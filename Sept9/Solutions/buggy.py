199f8992ce92177cc2805ab9208e5df3609fa7aa
William Darragh <william.james.darragh@live.mercer.edu>
2019-09-16 11:29:34 -0400

    started buggy problem

--- Sept9/Solutions/buggy.py
+++ Sept9/Solutions/buggy.py
@@ -1,5 +1,14 @@
 #buggy
 
+MOVES = {'L':(-1,0),'R':(1,0),'U':(0,-1),'D':(0,1)}
+
+def robot(moves):
+	global board,start
+	b = board.copy()
+	pos = start
+	for move in moves:
+		dr,dc = MOVES[move]
+		
 r,c = map(int,input().split())
 
 board = []
@@ -8,4 +17,15 @@ for i in range(r):
     board.append(input())
 
 moves = input()
+start = (0,0)
+
+for row in range(r):
+	for col in range(c):
+		if board[row][col] == 'R':
+			start = (r,c)
+			break
+	else:
+		continue
+	break
+
 
